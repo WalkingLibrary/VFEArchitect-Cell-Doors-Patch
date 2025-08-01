bd4a7aa52b5ba8e964765bb48ef236b8cfbc13de
Caleb <43940765+WalkingLibrary@users.noreply.github.com>
2025-08-01 12:46:11 -0600

    Refactored Mod Name Added Used DLL references
    Signed-off-by: Caleb <43940765+WalkingLibrary@users.noreply.github.com>

---
+++ VFEArchitectCellDoorsPatch/Source/ModEntry.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+using HarmonyLib;
+
+namespace VanillaFurnitureExpandedCellDoorsPatch
+{
+    public class ModEntry : Mod
+    {
+        public ModEntry(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("vanillafurnitureexpanded.cellDoorsPatch");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ VFEArchitectCellDoorsPatch/Source/Patch_Region_Allows.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+
+namespace VanillaFurnitureExpandedCellDoorsPatch
+{
+    [StaticConstructorOnStartup]
+    public static class PatchInit
+    {
+        static PatchInit()
+        {
+            var harmony = new Harmony("walkinglibrary.vfe.celldoorspatch");
+            harmony.Patch(
+                AccessTools.Method(typeof(Region), "Allows"),
+                transpiler: new HarmonyMethod(typeof(Patch_Region_Allows), nameof(Patch_Region_Allows.Transpiler))
+            );
+        }
+    }
+
+    public static class Patch_Region_Allows
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var list = new List<CodeInstruction>(instructions);
+            var field = AccessTools.Field(typeof(TraverseParms), "canBashDoors");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                yield return list[i];
+
+                if (list[i].LoadsField(field))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_1); // TraverseParms
+                    yield return CodeInstruction.LoadField(typeof(TraverseParms), "pawn");
+                    yield return new CodeInstruction(OpCodes.Ldarg_0); // Region
+                    yield return CodeInstruction.LoadField(typeof(Region), "door");
+                    yield return CodeInstruction.Call(typeof(Patch_Region_Allows), nameof(DontBashCellDoors));
+                }
+            }
+        }
+
+        public static bool DontBashCellDoors(bool canBash, Pawn pawn, Building_Door door)
+        {
+            Log.Message($"Checking bash: canBash={canBash}, isPrisoner={pawn?.IsPrisoner}, door={door?.def?.defName}");
+
+            if (!canBash)
+                return false;
+
+            if (pawn != null && pawn.IsPrisoner && door.def.defName == "VFEArch_CellDoor")
+            {
+                Log.Message("Blocking cell door bash attempt.");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
